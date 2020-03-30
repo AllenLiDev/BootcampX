ea80f023592f1611de2c3ee4dcf83b08823ba1bc
headhuntar <headhuntar@gmail.com>
2020-03-30 22:35:25 +0000

    added 2 new tables and data

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,16 @@
+CREATE TABLE assignments (
+  id serial PRIMARY KEY NOT NULL,
+  name varchar(255) NOT NULL,
+  content text,
+  day integer,
+  chapter integer,
+  duration integer
+);
+
+CREATE TABLE assignment_submissions (
+  id serial PRIMARY KEY NOT NULL,
+  assignment_id integer REFERENCES assignments (id) ON DELETE CASCADE,
+  student_id integer REFERENCES students (id) ON DELETE CASCADE,
+  duration integer,
+  submission_date date
+);
