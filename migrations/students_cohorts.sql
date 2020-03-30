88f1f951ca2385fa7268c2e5698aa4686257a900
headhuntar <headhuntar@gmail.com>
2020-03-30 21:54:29 +0000

    first commit - initial db

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,17 @@
+CREATE TABLE cohorts (
+  id serial PRIMARY KEY NOT NULL,
+  name varchar(255) NOT NULL,
+  start_date date,
+  end_date date
+);
+
+CREATE TABLE students (
+  id serial PRIMARY KEY NOT NULL,
+  name varchar(255) NOT NULL,
+  email varchar(255),
+  phone varchar(32),
+  github varchar(255),
+  start_date date,
+  end_date date,
+  cohort_id integer REFERENCES cohorts (id) ON DELETE CASCADE
+);
