3ebb5896c2b0b3e03d69605514f92a86939ae793
headhuntar <headhuntar@gmail.com>
2020-03-30 22:54:42 +0000

    join example

---
+++ 1_queries/rollover_students.sql
@@ -0,0 +1,12 @@
+SELECT
+  students.name AS student_name,
+  students.start_date AS student_start_date,
+  cohorts.name AS cohorts_name,
+  cohorts.start_date AS cohorts_start_Date
+FROM
+  students
+  JOIN cohorts ON cohort_id = cohort_id
+WHERE
+  cohorts.start_date != students.start_date
+ORDER BY
+  cohorts_name
